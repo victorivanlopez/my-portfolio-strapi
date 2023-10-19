c9881c8488fb6072f0e939f67a5aca10368e1ad7
Víctor Iván López González <victor_ivan2211@hotmail.com>
2023-10-19 13:51:13 -0600

    Updated: email structure

--- src/api/email/controllers/email.js
+++ src/api/email/controllers/email.js
@@ -7,9 +7,15 @@ module.exports = {
       await strapi.plugins['email'].services.email.send({
         to: 'victor_ivan22@hotmail.com',
         subject,
-        text: `
-          Nombre: ${name}\nTeléfono: ${tel}\nEmail: ${email}\nMensaje: ${message}
-        `,
+        html:
+          ` <html>
+              <h2>Nuevo mensaje de contacto</h2>
+              <p><strong>Nombre: </strong>${name}</p>
+              ${(tel.length > 0) ? `<p><strong>Teléfono: </strong>${tel}</p>` : ''}
+              <p><strong>Email: </strong>${email}</p>
+              <p><strong>Mensaje: </strong>${message}</p>
+            </html>
+          `,
       });
 
       ctx.send({ message: 'Correo enviado con éxito' });
